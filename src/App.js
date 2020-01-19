9ce2a82b15d00bbc09acf0a7755cb1ef50ebbc09
hoummani <yassinehoummani.tsi@gmail.com>
2020-01-20 00:06:49 +0100

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,58 +1,50 @@
 /* eslint-disable react/no-direct-mutation-state */
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import './App.scss'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    };
-    this.countUp = this.countUp.bind(this);
-    this.countInit = this.countInit.bind(this);
+export default function App (){
+  const [counter, setCounter] = useState(0);
+  
+  function countUp(){
+    setCounter(counter+1);
   }
-  countUp() {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-  countInit() {
-    this.setState({ counter: 0});
+  function countInit() {
+    setCounter(0);
   }
 
-  render() {
-    return (
-      <div className="container" style={{ marginTop: '25px', padding: '20px' }}>
-        <div className="has-padding-top-15">
-          <div className="has-padding-5" style={{ marginLeft: '10px' }}>
-            <h1 className="title">
-              Welcome to React Hooks Features
-            </h1>
-            <p className="">
-              React hooks let you use state and React lifecycle features without using class and React component lifecycle methods. They were introduced in React 16.8.
-            </p>
-            <br />
-            <h2 className="subtitle">Count : <strong>{this.state.counter}</strong></h2>
-            <div>
-              <button
-                className="button is-primary"
-                style={{ marginRight: '5px' }}
-                type="button"
-                onClick={this.countUp}
-              >
-                  <strong>CountUp</strong>
-              </button>
-              <button 
-                className="button is-secondary" 
-                type="button"
-                onClick={this.countInit}
-              >
-                  <strong>CountInit</strong>
-              </button>
-            </div>
+  return (
+    <div className="container" style={{ marginTop: '25px', padding: '20px' }}>
+      <div className="has-padding-top-15">
+        <div className="has-padding-5" style={{ marginLeft: '10px' }}>
+          <h1 className="title">
+            Welcome to React Hooks Features
+          </h1>
+          <p className="">
+            React hooks let you use state and React lifecycle features without using class and React component lifecycle methods. They were introduced in React 16.8.
+          </p>
+          <br />
+          <h2 className="subtitle">Count : <strong>{counter}</strong></h2>
+          <div>
+            <button
+              className="button is-primary is-outlined"
+              style={{ marginRight: '5px' }}
+              type="button"
+              onClick={countUp}
+            >
+                <strong>CountUp</strong>
+            </button>
+            <button 
+              className="button is-secondary" 
+              type="button"
+              onClick={countInit}
+            >
+                <strong>CountInit</strong>
+            </button>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
