8b57021a5f59443c189b0e30192297b476097631
hoummani <yassinehoummani.tsi@gmail.com>
2020-01-19 20:06:59 +0100

    adding bulma css framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 /* eslint-disable react/no-direct-mutation-state */
 import React, { Component } from 'react'
 
+import './App.scss'
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0
     };
+    this.countUp = this.countUp.bind(this);
+    this.countInit = this.countInit.bind(this);
   }
-  countUp = ()=> {
-    this.setState({ counter: ++this.state.counter });
+  countUp() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+  countInit() {
+    this.setState({ counter: 0});
   }
 
   render() {
     return (
-      <div>
-        <h1 style={{ textAlign: 'center', fontWeight: 'lighter' }}>Counter component</h1>
-        <h2 style={{ textAlign: 'center' }}>{ this.state.counter}</h2>
-        <button onClick={this.countUp} style={{ backgroundColor: 'blue', padding: '5px', color: 'white', fontWeight: 'bolder', border: '0px',fontSize:'22px', marginLeft: '40%', width:'250px' }}>CountUp</button>
+      <div className="container" style={{ marginTop: '25px', padding: '20px' }}>
+        <div className="has-padding-top-15">
+          <div className="has-padding-5" style={{ marginLeft: '10px' }}>
+            <h1 className="title">
+              Welcome to React Hooks Features
+            </h1>
+            <p className="">
+              React hooks let you use state and React lifecycle features without using class and React component lifecycle methods. They were introduced in React 16.8.
+            </p>
+            <br />
+            <h2 className="subtitle">Count : <strong>{this.state.counter}</strong></h2>
+            <div>
+              <button
+                className="button is-primary"
+                style={{ marginRight: '5px' }}
+                type="button"
+                onClick={this.countUp}
+              >
+                  <strong>CountUp</strong>
+              </button>
+              <button 
+                className="button is-secondary" 
+                type="button"
+                onClick={this.countInit}
+              >
+                  <strong>CountInit</strong>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
