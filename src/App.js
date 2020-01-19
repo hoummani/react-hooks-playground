22cbad86a2fdbb886e141f12af2d9bd91c9a24d0
hoummani <yassinehoummani.tsi@gmail.com>
2020-01-19 18:30:33 +0100

    firts setup to play with react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/no-direct-mutation-state */
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+  countUp = ()=> {
+    this.setState({ counter: ++this.state.counter });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center', fontWeight: 'lighter' }}>Counter component</h1>
+        <h2 style={{ textAlign: 'center' }}>{ this.state.counter}</h2>
+        <button onClick={this.countUp} style={{ backgroundColor: 'blue', padding: '5px', color: 'white', fontWeight: 'bolder', border: '0px',fontSize:'22px', marginLeft: '40%', width:'250px' }}>CountUp</button>
+      </div>
+    )
+  }
 }
 
-export default App;
